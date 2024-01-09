4536000d5d24f94241a221b5ca9480c77912c6d7
Loban Matin <loban.matin@gmail.com>
2024-01-09 19:52:40 +1030

    updated navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,20 @@
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="title">
-                <h1>Loban Matin</h1>
-                <h4>MECHATRONICS ENGINEERING STUDENT</h4>
-            </div>
-            <div className="links">
-                <a href="/">ABOUT</a>
-                <a href="/projects">PROJECTS</a>
-                <a href="/gallery">GALLERY</a>
-                <a href="/contact">CONTACT</a>
-            </div>
-        </nav>
+        <div className="nav-section">
+            <nav className="navbar">
+                <div className="accent"></div>
+                <div className="title">
+                    <h1 style={{fontWeight: "900"}}>Loban Matin</h1>
+                    <h4 style={{fontWeight: "100"}}>MECHATRONICS ENGINEERING STUDENT</h4>
+                </div>
+                <div className="links">
+                    <a href="/">ABOUT</a>
+                    <a href="/projects">PROJECTS</a>
+                    <a href="/gallery">GALLERY</a>
+                    <a href="/contact">CONTACT</a>
+                </div>
+            </nav>
+        </div>
     );
 }
  
